87bb6972fc284ffbdaf7dce5b864c08c001482d7
The Mini John <admin@phoenix-prime.gr>
2015-02-04 00:24:37 +0100

    Work on Observer

--- app/assets/javascripts/components/observer.js.jsx
+++ app/assets/javascripts/components/observer.js.jsx
@@ -6,26 +6,60 @@
 //     - Player
 
 var CurrentGame = React.createClass({
+
+  // get game info
+  loadGameData: function() {
+    $.ajax({
+      url: '/example.json',
+      dataType: 'json',
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error('#GET Error', status, err.toString());
+      }.bind(this)
+    });
+  },
+
+  getInitialState: function(){
+    return {data: []};
+  },
+
+  componentDidMount: function() {
+    this.loadGameData();
+  },
+
   render: function() {
     return (
       <div className="CurrentGame">
         <h1> Current Game Information</h1>
-        <PlayerList />
+        <PlayerList data={this.state.data}/>
       </div>
     );
   }
 });
 
+
+
 var PlayerList = React.createClass({
+
+  // here we access with 'this.props' 
+  // the data from parent Component
+
   render: function() {
+
+    console.log(this.props.data);
+
     return (
       <div className="PlayerList">
-        I'm the Player List
-        <Player />
+        I'm the Player List {this.props.data}
+        <Player author="The Mini John" />
       </div>
     );
   }
 });
+
+
 
 var Player = React.createClass({
   render: function() {
@@ -37,12 +71,14 @@ var Player = React.createClass({
   }
 });
 
+
+
 // Render
 $(document).on("page:change", function() {
   var $content = $("#currentGameBox");
   if ($content.length > 0) {
     React.render(
-      <CurrentGame />,
+      <CurrentGame data="/example.json" />,
       document.getElementById('currentGameBox')
     );
   }
