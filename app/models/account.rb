fc5fc2d259ac59b4c6860b9811dd31e95cd597e7
The Mini John <admin@phoenix-prime.gr>
2015-02-04 22:40:17 +0100

    Add Account Update - Active Admin

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 require 'lol'
+require 'open-uri'
 class Account < ActiveRecord::Base
 
   # Assocations
@@ -16,19 +17,40 @@ class Account < ActiveRecord::Base
     end
 
     # Update Account
-    binding.pry
     data = client.stats.summary(league_id)
     stats = data[3]
 
     # Just to be sure check if its the correct Type
     if stats.player_stat_summary_type == "RankedSolo5x5"
-      self.wins   = stats.wins
-      self.losses = stats.losses
+      self.wins                         = stats.wins
+      self.losses                       = stats.losses
+      self.total_champion_kills         = stats.aggregated_stats['total_champion_kills']
+      self.total_turrets_killed         = stats.aggregated_stats['total_turrets_killed']
+      self.total_minion_kills           = stats.aggregated_stats['total_minion_kills']
+      self.total_neutral_minions_killed = stats.aggregated_stats['total_neutral_minions_killed']
+      self.total_assists                = stats.aggregated_stats['total_assists']
+
+      # save
+      self.save
     end
 
+    # Get Division Info
 
+    league = JSON.load(open("https://eune.api.pvp.net/api/lol/#{self.server}/v2.5/league/by-summoner/#{self.league_id}/entry?api_key=#{Settings.rito.api_key}"))
 
+    binding.pry
 
+    self.tier           = league.first[1][0]['tier']
+    self.division       = league.first[1][0]['entries'][0]['division']
+    self.division_name  = league.first[1][0]['name']
+    self.league_points  = league.first[1][0]['entries'][0]['leaguePoints']
+    self.is_veteran     = league.first[1][0]['entries'][0]['isVeteran']
+    self.is_inactive    = league.first[1][0]['entries'][0]['isInactive']
+    self.is_hot_streak  = league.first[1][0]['entries'][0]['isHotStreak']
+    self.is_fresh_blood = league.first[1][0]['entries'][0]['IsFreshBlood']
+
+    # save
+    self.save
 
   end
 
