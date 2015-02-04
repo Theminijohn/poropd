0a2d9732e733c96c5e084f356433a5760d6bca7d
The Mini John <admin@phoenix-prime.gr>
2015-02-04 19:08:52 +0100

    Update Def

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,13 @@ class Account < ActiveRecord::Base
   # Assocations
   belongs_to :streamer
 
+  # Update Account Information
+  def update_intel
+    # Update Account
+    client = Lol::Client.new Settings.rito.api_key, {region: 'na'}
+
+    client.league.get(self.league_id)
+    # => Lol::League
+  end
+
 end
